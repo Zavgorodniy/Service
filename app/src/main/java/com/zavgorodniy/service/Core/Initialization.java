9abf543ee894e6fe1232d977b923a54f8b93ffc8
Zavgorodniy <zavgorodniy.nick@gmail.com>
2016-03-02 13:09:20 +0200

    Initial commit

---
+++ app/src/main/java/com/zavgorodniy/service/Core/Initialization.java
@@ -0,0 +1,11 @@
+package com.zavgorodniy.service.Core;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nick on 02.03.16.
+ */
+public class Initialization {
+
+    ArrayList<String> genres;
+}
---
+++ app/src/main/java/com/zavgorodniy/service/Core/Item.java
@@ -0,0 +1,8 @@
+package com.zavgorodniy.service.Core;
+
+/**
+ * Created by nick on 02.03.16.
+ */
+public class Item {
+
+}
